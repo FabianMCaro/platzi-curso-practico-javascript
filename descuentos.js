024fd6ee7623c4092421724f214265f15f755781
Fabian Caro <fabiancaro101@gmail.com>
2022-10-08 08:34:13 -0500

    Agregamos la opcoion de descuento mediante cupones

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,21 @@
 //const precioOriginal = 120;
 //const descuento = 18;
 
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -12,9 +27,36 @@ function onClickButtonPriceDiscont(){
     const inputDiscount = document.getElementById("InputDiscount");
     const priceValue = inputPrice.value;
     const discountValue = inputDiscount.value;
+    
     const preioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + preioConDescuento;
+
+    
+}
+
+function onClickButtonPriceCoupon(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    console.log(couponValue)
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + "no es válido");
+    }else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
 }
 //console.log({
 //    precioOriginal,
